2d97a283e55f4fd0ae331af1f47732225b3679ab
Nick Centola <nickcentola@ROTIs-MBP.lan>
2019-08-18 16:13:46 -0500

    brightloom api

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='brandibble',
+      version='0.1',
+      description='Brandibble API wrapper',
+      url='http://github.com/ncentola/brandibble',
+      author='Nick Centola',
+      author_email='centola.nick@gmail.com',
+      license='MIT',
+      packages=find_packages()
+      )
